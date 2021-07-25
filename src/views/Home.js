af5c2b77ce2e74b038d3967af21c9ad551a707f7
Giffari Alfarizy <giffarialfarizy@gmail.com>
2021-07-25 21:17:53 +0700

    first push

---
+++ src/views/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>SENIKURNI</h1>
+      <iframe
+        title='Spline'
+        src='https://my.spline.design/senikurnicopy-c32be502baecbd1b69cc31d0a860339f/'
+        frameborder='0'
+        width='100%'
+        height='100%'
+        position='absolute'
+      ></iframe>
+    </div>
+
+    // <iframe src='https://my.spline.design/senikurnicopy-c32be502baecbd1b69cc31d0a860339f/' frameborder='0' width='100%' height='100%'></iframe>
+  );
+}
+
+export default Home;
