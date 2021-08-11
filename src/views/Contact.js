0d5d758bfcf4db087643094fa40c23578589631c
Giffari Alfarizy <giffarialfarizy@gmail.com>
2021-08-11 18:42:58 +0700

    minor fix

--- src/views/Contact.js
+++ src/views/Contact.js
@@ -3,7 +3,7 @@ function Contact() {
 		<div className="h-screen overflow-hidden" id="blog">
 			<div className="p-5">
 				<h1 className="text-2xl font-bold text-center">Let's Connect</h1>
-				<p className="text-center m-64">
+				<p className="text-center m-8">
 					Feel free to reach out for collaborations or just a friendly hello 😀<br></br>
 					<span className="text-primary font-bold cursor-pointer">adhi.kurni@gmail.com</span>
 				</p>
